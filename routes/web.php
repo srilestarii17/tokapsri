4d771b7a9404537c1b8a339e5422a9360bece45d
Sri Lestari <sri170102@gmail.com>
2019-02-07 14:33:46 +0700

    Video eps 5-7

--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,15 @@ Route::prefix('admin')->group(function(){
 	Route::get('/', function(){
 		return view('admin.pages.dashboard');
 	})->name('admin.home');
+
+	Route::prefix('user')->group(function(){
+		Route::get('/setting','UserSettingController@form')->name('admin.user.setting');
+		Route::post('/setting','UserSettingController@update');
+	});
 });
 });
 
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
