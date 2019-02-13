68d7dbe6cfab08f20a6f3bc3595574c91da7dc44
Sri Lestari <sri170102@gmail.com>
2019-02-13 10:49:44 +0700

    Video eps 10

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::prefix('admin')->group(function(){
 	})->name('admin.home');
 
 	Route::prefix('user')->group(function(){
+		Route::get('/','UserController@daftar')->name('admin.user');
 		Route::get('/setting','UserSettingController@form')->name('admin.user.setting');
 		Route::post('/setting','UserSettingController@update');
 	});
